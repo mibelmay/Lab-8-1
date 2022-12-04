9de3ded2c782b15f3c0ff0e47ed5542f72556b4b
Stacy <zemcovannastasia11@gmail.com>
2022-12-04 22:40:26 +0500

    added class Subtitle

---
+++ Subtitles/Subtitle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Timers;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Subtitles
+{
+    internal class Subtitle
+    {
+        public int StartTime { get; set; }
+        public int EndTime { get; set; }
+        public string Text { get; set; }
+        public string Position { get; set; }
+        public string Color { get; set; }
+
+        public Subtitle(int startTime, int endTime, string text, string position = "Bottom", string color = "White")
+        {
+            StartTime = startTime;
+            EndTime = endTime;
+            Text = text;
+            Position = position;
+            Color = color;
+        }
+
+    }
+}
